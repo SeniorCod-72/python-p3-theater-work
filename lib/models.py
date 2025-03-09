031289150cc61d275da9ef1115af600e0d8ba91a
SeniorCod-72 <wwekesa72@gmail.com>
2025-03-09 23:39:13 +0300

    relationships added

--- lib/models.py
+++ lib/models.py
@@ -9,6 +9,7 @@ class Role(Base):
     __tablename__ = "roles"
     id = column(Integer, primary_key = True)
     character_name = column(String)
+    auditions = relationship('Audition', back_populates='role')
 
 
     class Audition(Base):
@@ -20,4 +21,12 @@ class Role(Base):
         hired = column(Boolean, default = False)
         role_id = column(Integer, ForeignKey("roles.id"))
         
+        role = relationship('Role' back_populates='auditions')
+
+
+        
+
+
+
+
 
